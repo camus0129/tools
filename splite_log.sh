6aea1ca234697fa4a6ef1d762500e6bf26ee0836
camus0129 <shenhao0129@gmail.com>
2020-03-12 16:50:46 +0800

    Fixed calc error for last paragraph

--- splite_log.sh
+++ splite_log.sh
@@ -7,9 +7,11 @@ echo "splite key words $2 "
 
 #get max num for splite
 num=`grep -c $2 $1`
+let num=num+1
 echo "splite $1 to $num files"
 
 grep -n $2 $1 | awk -F : '{print $1}' > line.txt
+wc -l $1| awk '{print $1}' >> line.txt
 
 i=1
 start=1
@@ -17,7 +19,7 @@ while read end
 do
 let end=end-1
 sed -n "$start,$end p" $1  >$i.txt
-rm -rf $i.txt
+#rm -rf $i.txt
 let i=i+1
 let start=end+1
 done < line.txt
